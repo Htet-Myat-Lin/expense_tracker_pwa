b371ed9421c9abcf222d8b21e6abea24af509605
Htet-Myat-Lin <hlin96741@gmail.com>
2026-08-30 20:24:12 +0630

    config

--- src/api/axios.instance.ts
+++ src/api/axios.instance.ts
@@ -1,10 +1,12 @@
-import axios from "axios";
+import axios from 'axios'
+
+const apiBaseUrl = import.meta.env.VITE_API_URL
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'  
-    }
+  baseURL: `${apiBaseUrl.replace(/\/$/, '')}/api`,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  }
 })
